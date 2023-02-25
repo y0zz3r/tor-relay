12842034e6c5cd14bccc648acf6b62ad05cf73cb
y0zz3r <117989718+y0zz3r@users.noreply.github.com>
2023-02-25 21:48:26 +0000

    Update tor-relay-setup.sh

--- tor-relay-setup.sh
+++ tor-relay-setup.sh
@@ -1,36 +1,51 @@
 #!/bin/bash
 
+set -euo pipefail
+
+# Check if the script is being run as root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
 # Prompt user for nickname for the Tor relay
 read -p "Enter a nickname for the Tor relay: " RELAY_NICKNAME
 
 # Update packages and install required packages
-sudo apt-get update
-sudo apt-get install -y apt-transport-https gnupg curl ufw
+apt-get update
+apt-get upgrade -y
+apt-get install -y apt-transport-https gnupg curl ufw
 
 # Configure the firewall
-sudo ufw default deny incoming
-sudo ufw default allow outgoing
-sudo ufw allow ssh
-sudo ufw allow 9001/tcp
-sudo ufw enable
+ufw default deny incoming
+ufw default allow outgoing
+ufw allow ssh
+ufw allow 9001/tcp
+ufw enable
 
-# Add Tor Project signing key and repository
-curl https://deb.torproject.org/key.asc | sudo gpg --import -
+# Verify Tor Project signing key and add repository
+curl https://keys.openpgp.org/vks/v1/by-fingerprint/0xEF6E286DDA85EA2A4BA7DE684E2C6E8793298290 | gpg --import -
+curl https://deb.torproject.org/torproject.org/A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89.asc | gpg --import -
+gpg --verify /tmp/torproject.asc /tmp/torproject.asc.sig
 echo "deb https://deb.torproject.org/torproject.org $(lsb_release -cs) main" | sudo tee -a /etc/apt/sources.list.d/tor.list
 echo "deb-src https://deb.torproject.org/torproject.org $(lsb_release -cs) main" | sudo tee -a /etc/apt/sources.list.d/tor.list
 
-# Update packages and install Tor
-sudo apt-get update
-sudo apt-get install -y tor
+# Check if Tor is already installed
+if ! command -v tor &> /dev/null
+then
+    # Install Tor
+    apt-get update
+    apt-get install -y tor
+fi
 
 # Configure Tor relay settings
-sudo sed -i "s/#Nickname\ \".*\"/Nickname $RELAY_NICKNAME/" /etc/tor/torrc
-sudo sed -i "s/#ORPort\ \d+/ORPort 9001/" /etc/tor/torrc
-sudo sed -i "s/#ExitPolicy\ accept\ \*\:\*/ExitPolicy reject \*:\*/" /etc/tor/torrc
+sed -i "s/#Nickname\ \".*\"/Nickname $RELAY_NICKNAME/" /etc/tor/torrc
+sed -i "s/#ORPort\ \d+/ORPort 9001/" /etc/tor/torrc
+sed -i "s/#ExitPolicy\ accept\ \*\:\*/ExitPolicy reject \*:\*/" /etc/tor/torrc
 
 # Restart Tor
-sudo systemctl restart tor
+systemctl restart tor
 
 # Display the Tor relay fingerprint and bandwidth information
-echo "Tor Relay Fingerprint: $(sudo cat /var/lib/tor/$RELAY_NICKNAME/fingerprint)"
-echo "Tor Relay Bandwidth: $(sudo cat /var/lib/tor/$RELAY_NICKNAME/bandwidth)"
+echo "Tor Relay Fingerprint: $(cat /var/lib/tor/$RELAY_NICKNAME/fingerprint)"
+echo "Tor Relay Bandwidth: $(cat /var/lib/tor/$RELAY_NICKNAME/bandwidth)"
